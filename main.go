ff36210dfce8c48c76e5302323be1027cafbc566
Racxshan Nagalingam <510460@student.fontys.nl>
2023-03-08 20:42:26 +0100

    Versie met Parameter

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ func main() {
 	var getal int
 	fmt.Println("Voer een getal in:")
 	fmt.Scan(&getal)
-	if getal < 0 {
-		fmt.Println("Geen NEGATIEVE getallen!! Voer een nieuwe getal in:")
-		fmt.Scan(&getal)
-	}
+	forloop(getal)
+}
+
+func forloop(getal int) {
 	for i := 0; i < getal; i++ {
 		fmt.Println("alarm!", i)
 	}
